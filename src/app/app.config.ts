9e238d6da79501effca6b71f6f057452a5ec9d4c
Andy Sullcaray <i2414593@continental.edu.pe>
2026-06-19 17:41:42 -0500

    taller-complet5

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideSweetAlert2 } from '@sweetalert2/ngx-sweetalert2';
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideSweetAlert2({
-      fireOnInit: false,
-      dismissOnDestroy: true,
-    }),
   ],
 };
